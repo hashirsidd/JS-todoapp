a2d858d455eae5da7138765388365cd1fec1851f
hahsir <hahsir.contact@gmail.com>
2020-07-27 15:42:15 +0500

    made it prepand added animation to buttons

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 
-
-function addHtml(todo){
-  
-  var html = "<li class='mb-3'><i class='fa fa-angle-right pr-4'></i><div class='text'>"+todo+"</div> <a onclick='del(this)'><i class='fa fa-trash delete'></i></a></li>"
-  document.getElementById('items').innerHTML += html;
+function addHtml(todo) {
+  var html = '<li class="mb-3"> <a onclick="done(this)" id="linkdone"><i id="square" class="fa fa-square-o pr-4"></i><div id="val" class="text">' + todo + '</div></a> <a onclick="del(this)" class="del"><i class="fa fa-trash delete"></i></a> </li> ';
+  document.getElementById('items').innerHTML = html + document.getElementById('items').innerHTML;
 }
-
-function doit_onkeypress(event){
-  if (event.keyCode == 13 || event.which == 13){
-      addTodos();
+function done(a) {
+  a.setAttribute('onclick', 'undo(this)');
+  var i = a.firstChild;
+  i.classList.replace('fa-square-o', 'fa-check-square-o');
+  var div = a.childNodes[1];
+  div.classList.add('line');
+}
+function undo(a) {
+  a.setAttribute('onclick', 'done(this)');
+  var i = a.firstChild;
+  i.classList.replace('fa-check-square-o', 'fa-square-o');
+  var div = a.childNodes[1];
+  div.classList.remove('line');
+}
+function doit_onkeypress(event) {
+  if (event.keyCode == 13 || event.which == 13) {
+    addTodos();
   }
 }
 function addTodos() {
-  item="";
-  
+  item = "";
+
   item = document.getElementById('input-item').value;
   if (item != "") {
     item = item.toLowerCase();
     var f = item[0].toUpperCase();
     var fa = item.slice(1);
-    addHtml(f+fa);
+    addHtml(f + fa);
     document.getElementById('input-item').value = "";
   }
 }
 
-function del(a){
+function del(a) {
   console.log(a.parentElement.parentElement.innerHTML);
   a.parentElement.parentElement.removeChild(a.parentElement);
-  
+
 }
 
 
